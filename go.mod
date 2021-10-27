c9b6dacf15a600b18c898600200f05024f186147
Michael Collado <mike@datakin.com>
2021-10-27 12:37:04 -0700

    Update to work with AWS lambda

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/PeladoCollado/reviewers_court
 go 1.15
 
 require (
+	github.com/aws/aws-lambda-go v1.27.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"crypto/rsa"
 	"encoding/json"
 	"fmt"
-	"github.com/google/go-github/github"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/google/go-github/github"
 	"go.uber.org/zap"
-	"net/http"
+	"golang.org/x/oauth2"
 	"os"
 	"time"
-	"golang.org/x/oauth2"
 )
 
 const APP_ID = "myAppId"
 
 const REVIEWER_COURT = "reviewers_court"
 const AUTHOR_COURT = "authors_court"
+
 var ReviewerLabels = []string{REVIEWER_COURT, AUTHOR_COURT}
 
 var courtLabels = make(map[string]*github.Label)
@@ -34,19 +37,18 @@ func init() {
 	var err error
 	pem, err = jwt.ParseRSAPrivateKeyFromPEM(pemBytes)
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"Invalid PEM content- unable to parse %w", err)
+		fmt.Fprintf(os.Stderr, "Invalid PEM content- unable to parse %w", err)
 		os.Exit(1)
 	}
 }
 
 type TokenSource struct {
-
 }
 
 func (t TokenSource) Token() (*oauth2.Token, error) {
 	claim := jwt.StandardClaims{
-		Issuer: APP_ID,
-		IssuedAt: time.Now().Unix(),
+		Issuer:    APP_ID,
+		IssuedAt:  time.Now().Unix(),
 		ExpiresAt: time.Now().Add(time.Minute).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.GetSigningMethod("RS256"), claim)
@@ -69,61 +71,65 @@ func main() {
 	httpClient := oauth2.NewClient(context.Background(), TokenSource{})
 	client = github.NewClient(httpClient)
 
-	// handle incoming events
-	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
-		decoder := json.NewDecoder(r.Body)
-		ctx := context.Background()
-		log.Debugf("Received new %s event- %v", r.Method, r.Header)
-		if r.Method == "POST" {
-			switch githubEvent := r.Header.Get("X-GitHub-Event"); githubEvent {
-			case "pull_request":
-				event := &github.PullRequestEvent{}
+	lambda.Start(handleEvent)
+}
 
-				loadLabels(context.Background(), *event.Repo.Owner.Name, *event.Repo.Owner.Name)
-				decoder.Decode(event)
-				action := *event.Action
-				if action == "review_requested" || action == "opened" {
-					if event.RequestedReviewer != nil || len(event.PullRequest.RequestedReviewers) > 0 {
-						changeCourt(ctx, REVIEWER_COURT, event.PullRequest)
-					}
-				} else if action == "unlabeled" {
-					courtLabelPresent := false
-					for i := range event.PullRequest.Labels {
-						if labelIds[*event.PullRequest.Labels[i].ID] {
-							courtLabelPresent = true
-							break
-						}
-					}
-					// the PR was manually labeled
-					if courtLabelPresent {
-						log.Info("PR was manually labeled- no action taken")
+func handleEvent(ctx context.Context, r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	decoder := json.NewDecoder(bytes.NewBufferString(r.Body))
+	log.Debugf("Received new %s event- %v", r.HTTPMethod, r.Headers)
+	var err error
+	if r.HTTPMethod == "POST" {
+		switch githubEvent := r.Headers["X-GitHub-Event"]; githubEvent {
+		case "pull_request":
+			event := &github.PullRequestEvent{}
+
+			loadLabels(context.Background(), *event.Repo.Owner.Name, *event.Repo.Owner.Name)
+			decoder.Decode(event)
+			action := *event.Action
+			if action == "review_requested" || action == "opened" || action == "reopened" {
+				if event.RequestedReviewer != nil || len(event.PullRequest.RequestedReviewers) > 0 {
+					err = changeCourt(ctx, REVIEWER_COURT, event.PullRequest)
+				}
+			} else if action == "unlabeled" {
+				courtLabelPresent := false
+				for i := range event.PullRequest.Labels {
+					if labelIds[*event.PullRequest.Labels[i].ID] {
+						courtLabelPresent = true
 						break
 					}
-
-					// author has unlabeled- assume reviewer's court
-					var court string
-					if event.Sender.ID == event.PullRequest.User.ID {
-						court = REVIEWER_COURT
-					} else {
-						court = AUTHOR_COURT
-					}
-					changeCourt(ctx, court, event.PullRequest)
 				}
-			case "pull_request_review":
-				event := &github.PullRequestReviewEvent{}
-				err := decoder.Decode(event)
-				if err != nil {
-					w.WriteHeader(400)
-					w.Write([]byte(fmt.Sprintf("Unable to decode JSON - %w", err)))
+				// the PR was manually labeled
+				if courtLabelPresent {
+					log.Info("PR was manually labeled- no action taken")
+					break
 				}
-				changeCourt(ctx, REVIEWER_COURT, event.PullRequest)
-			case "pull_request_review_comment":
 
-
+				// author has unlabeled- assume reviewer's court
+				var court string
+				if event.Sender.ID == event.PullRequest.User.ID {
+					court = REVIEWER_COURT
+				} else {
+					court = AUTHOR_COURT
+				}
+				err = changeCourt(ctx, court, event.PullRequest)
 			}
+		case "pull_request_review":
+			event := &github.PullRequestReviewEvent{}
+			err = decoder.Decode(event)
+			if err != nil {
+				return events.APIGatewayProxyResponse{StatusCode: 400, Body: fmt.Sprintf("Unable to decode JSON - %w", err)}, nil
+			}
+			err = changeCourt(ctx, REVIEWER_COURT, event.PullRequest)
+		case "pull_request_review_comment":
+
 		}
-	})
-	http.ListenAndServe("localhost:8080", http.DefaultServeMux)
+	}
+	if err != nil {
+		log.Errorf("Error handling request %w", err)
+		return events.APIGatewayProxyResponse{StatusCode: 500, Body: err.Error(), IsBase64Encoded: false}, err
+	} else {
+		return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+	}
 }
 
 func loadLabels(ctx context.Context, owner string, repo string) error {
